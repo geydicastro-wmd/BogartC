154d60050718c41af7ac27f9be345f63496e9dcd
geydicastro-wmd <geydi.castro@wmdcr.com>
2026-04-20 12:23:37 -0600

    remove pwa plugin (fix vercel build)

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,14 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  // Carga las variables de entorno del .env
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
     plugins: [
       react(),
-      VitePWA({
-        registerType: 'autoUpdate',
-        devOptions: { enabled: true },
-        workbox: {
-          globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-          navigateFallback: 'index.html',
-          cleanupOutdatedCaches: true,
-          clientsClaim: true,
-          skipWaiting: true
-        },
-        manifest: {
-          name: 'Bogart',
-          short_name: 'Bogart',
-          description: 'Bogart',
-          theme_color: '#d29f53',
-          display: 'standalone',
-          display_override: ["window-controls-overlay", "standalone"],
-          start_url: '/',
-          icons: [
-            { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-            { src: '/icon-512.png', sizes: '512x512', type: 'image/png' }
-          ]
-        }
-      })
+
     ],
 
     // Proxy nivel raíz usando la variable de entorno
