4233658c617844c1cdf44fff8a01e7a15fc90d72
geydicastro-wmd <geydi.castro@wmdcr.com>
2026-04-20 13:40:06 -0600

    fix env variables for vite

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()]
+})
+
+
+{/* 
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
     plugins: [
-      react()
-    ]
-  }
-})
+      react(),
+
+    ],
+
     // Proxy nivel raíz usando la variable de entorno
     server: {
       proxy: {
@@ -31,3 +41,4 @@ export default defineConfig(({ mode }) => {
     }
   }
 })
+*/}
